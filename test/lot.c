c8669c5fe5d5401a9c425c30686baa01a925a883
NanguRepo <nanguthenangu@gmail.com>
2025-11-25 11:37:33 +0100

    create 1 test for lot

--- test/lot.c
+++ test/lot.c
@@ -1,5 +1,5 @@
 #include "unity.h"
-#include "data.h"
+#include "lot.h"
 
 void setUp()
 {
@@ -9,13 +9,18 @@ void tearDown()
 {
 }
 
-void test_add_2_and_2(void)
+void test_create_lot(void)
 {
-	TEST_ASSERT_EQUAL_INT(4, addfn(2, 2));
+  Lot* lot = create_lot(30, 3, 0, 0);
+  TEST_ASSERT_EQUAL_INT(lot->space_count, 3); 
+  TEST_ASSERT_EQUAL_INT(lot->path_count, 30); 
+  TEST_ASSERT_EQUAL_INT(lot->up_count, 0); 
+  TEST_ASSERT_EQUAL_INT(lot->down_count, 0); 
 }
+
 int main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_add_2_and_2);
+	RUN_TEST(test_create_lot);
 	return UNITY_END();
 }
